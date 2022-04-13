798933f07e144d84ec564b43f648832a24faf54d
leon <leon960623@gmail.com>
2022-04-13 20:50:51 +0800

    855445 詹豊洋 成績AVG

---
+++ 成績AVG.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+vector <int> score {};
+int sc , std = 0 , t = 0;
+	
+while (true){
+	cin >> sc;
+	t += sc;
+	if (sc == 0){
+		break;
+	}
+	else{
+		score.push_back(sc);
+		}
+	}
+	sort(score.begin(), score.end());
+  int n;
+  n = score.size();
+	cout << t/score.size() << " " << score[n-1] << " " << score[0] ;
+	}
+ 
