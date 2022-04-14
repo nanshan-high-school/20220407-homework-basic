a9e01a4ed8f594aa6a2007d4b67429595fb6fad7
ThatShark <90843512+ThatShark@users.noreply.github.com>
2022-04-14 19:32:00 +0800

    更新更新

--- 855242/Average_vector.cpp
+++ 855242/Average_vector.cpp
@@ -16,7 +16,7 @@ int main() {
   
   int seatNumber = 1, minScore = 101, maxScore = 0;
   vector<int> minPerson = {}, maxPerson = {};
-  for (int i: scores) {
+  for (auto i: scores) {
     if (i >= maxScore) {
       if (i > maxScore) {
         maxScore = i;
@@ -36,8 +36,8 @@ int main() {
 
   cout << "班級總分為" << totalScore << "分\n";
   cout << "班級總平均為" << (float)totalScore / scores.size() << "分\n";
-  for (int i : maxPerson) cout << " " << i ;
+  for (auto i : maxPerson) cout << " " << i ;
   cout << "號為最高分" << maxScore << "分 ; ";
-  for (int i : minPerson) cout << " " << i;
+  for (auto i : minPerson) cout << " " << i;
   cout << "號為最低分" << minScore << "分";
 }
